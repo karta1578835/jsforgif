f0e3219851bfa8728579773029edad3c4143cf4a
karta1578835 <karta1578835@gmail.com>
2019-05-17 01:58:47 +0800

    bf1

---
+++ 2019up40/hw/hw4/main-win.js
@@ -0,0 +1,64 @@
+var shelljs = require('shelljs');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/hw4.html');
+});
+
+app.get ('/api', function (req, res) {
+
+    //var K = req.query.K;
+    //var t = req.query.t;
+    var minX = req.query.minX;
+    var minY = req.query.minY;
+    var maxX = req.query.maxX;
+    var maxY = req.query.maxY;
+    var cX = req.query.cX;
+    var cY = req.query.cY;
+    var r = req.query.r;
+	shelljs.exec('hw4.exe ' + cX
+        + ' ' + cY
+        + ' ' + r
+        + ' ' + minX
+        + ' ' + minY
+        + ' ' + maxX
+        + ' ' + maxY, function (status, output) {
+        console.log('Exit status:', status);
+        console.log('Program output:', output);
+
+        var output = {
+            status: status,
+            output: output
+        };
+
+
+        /*
+          The response header for supporting CORS:
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type"
+        */
+
+        res.writeHead(200, {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type"
+        });
+
+        res.write(JSON.stringify(output));
+        res.end();
+
+    });
+
+});
+
+
+// or simply
+// app.listen (1337); 
+// will do
+
+var server = app.listen(1337, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('server started on http://' + host + ':' + port);
+});
